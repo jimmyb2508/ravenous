64c3a9314ac7baf3bab27494db5fbd8044ee68eb
James Barrington <james.barrington1985@gmail.com>
2020-08-21 22:58:42 +0100

    Started work on Search Bar Component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,3 +2,16 @@ import React from 'react';
 
 import './SearchBar.css';
 
+const sortByOptions {
+  bestMatch: 'best_match',
+  highestRated: 'rating',
+  mostReviewed: 'review_count'
+}
+
+class SearchBar extends React.Component {
+  renderSortByOptions() {
+    
+  }
+}
+
+export default SearchBar;
